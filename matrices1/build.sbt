f4e8cb4b963a1bab006fba6c2d832ef16a5d973a
NoName <nda85@ya.ru>
2016-03-11 22:14:52 +0400

    actual collections quirks

---
+++ matrices1/build.sbt
@@ -0,0 +1,6 @@
+name := "matrices1"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+    
--- matrices1/src/main/scala/Row.scala
+++ matrices1/src/main/scala/Row.scala
@@ -1,6 +1,16 @@
 /**
   * Created by noname on 11.03.16.
   */
-class Row {
+class Row[T](elems: T*)(implicit numeric: Numeric[T]) extends Cloneable {
+  import numeric._
 
+  val length = elems.length
+
+  def *(a: T): Row[T] = {
+    new Row(elems.map((x) => x * a).toSeq.toArray)
+  }
+
+  override def clone: Row[T] = new Row[T](elems.clone)
+
+  override def toString: String = elems.mkString("(", " ", ")")
 }
