85a7451e5c0730df3e65fe82e2b85a1196487d8b
dnoskov <nda85@ya.ru>
2016-03-14 02:55:03 +0400

    matrices1 done

--- matrices1/src/main/scala/Matrix.scala
+++ matrices1/src/main/scala/Matrix.scala
@@ -11,20 +11,21 @@ object Matrix {
 }
 
 
-case class Matrix[T](e: Row[T]*)(implicit fractional: Fractional[T]) extends Cloneable {
+case class Matrix[T](e: Row[T]*)(implicit fractional: Fractional[T], tag: ClassTag[T]) extends Cloneable {
   import fractional._
+  import tag._
 
-  val elems = e.toArray
+  val rows = e.toArray
 
-  val dim = elems.length
+  val dim = rows.length
 
-  def this(e: Array[Row[T]])(implicit fractional: Fractional[T], tag: ClassTag[T]) =
-    this(e: _*)
-
-  def this(dim: Int) = {
-    this(new Array[Row[T]](dim))
-    for (i <- this.elems.indices) this.elems(i) = new Row[T](dim)
-  }
+  //  def this(e: Array[Row[T]])(implicit fractional: Fractional[T], tag: ClassTag[T]) =
+  //    this(e: _*)
+  //
+  //  def this(dim: Int)(implicit fractional: Fractional[T], tag: ClassTag[T])= {
+  //    this(new Array[Row[T]](dim))
+  //    for (i <- this.rows.indices) this.rows(i) = new Row(new Array[T](dim))
+  //  }
 
   def det: T = {
     val copy = this.clone
@@ -49,23 +50,27 @@ case class Matrix[T](e: Row[T]*)(implicit fractional: Fractional[T]) extends Clo
       result *= copy(i,i)
       val normrow = copy(i) / copy(i,i)
       for (j <- i+1 until dim) {
-        copy.elems(j) = copy.elems(j) - (normrow * copy.elems(j)(i))
+        copy.rows(j) = copy.rows(j) - (normrow * copy.rows(j)(i))
       }
     }
 
-    result
+    result * mult
   }
 
-  def norm: T = elems.maxBy((r) => r.norm).norm
+  def norm: T = rows.maxBy((r) => r.norm).norm
 
-  private def apply(i: Int): Row[T] = elems(i)
+  private def apply(i: Int): Row[T] = rows(i)
 
-  def apply(i: Int, j: Int): T = elems(i)(j)
+  def apply(i: Int, j: Int): T = rows(i)(j)
+
+  def apply(i: Int, j: Int, x: T) = {
+    rows(i).elems(j) = x
+  }
 
   private def swap(i: Int, j: Int): Unit = {
-    val tmprow = elems(i)
-    elems(i) = elems(j)
-    elems(j) = tmprow
+    val tmprow = rows(i)
+    rows(i) = rows(j)
+    rows(j) = tmprow
   }
 
   def isSymmetric: Boolean = {
@@ -88,22 +93,56 @@ case class Matrix[T](e: Row[T]*)(implicit fractional: Fractional[T]) extends Clo
     return true
   }
 
-  def row(i: Int): Array[T] = elems(i).elems.toArray
+  def row(i: Int): Array[T] = rows(i).e.toArray
 
   def col(j: Int): Array[T] = (for (i <- 0 until dim) yield this(i,j)).toArray
 
   def *(m: Matrix[T]): Matrix[T] = {
-    var result = new Matrix[T](dim)
-    for (i <- 0 until dim; j <- 0 until dim)
-      result.elems(i) elems j = 
-  }
-
-  override def toString: String = {
-    val result = (for (r <- elems) yield r.toString).mkString("\n")
+    val result = new Matrix(new Array[Row[T]](dim): _*)
+    for (i <- result.rows.indices) {
+      result.rows(i) = Row(new Array[T](dim): _*)
+      for (j <- result.rows(i).elems.indices)
+        result.rows(i).elems(j) = row(i).zip(m.col(j)) map {case (x,y) => x * y} reduceLeft { (a,b) => a + b}
+    }
     result
   }
 
-  override def clone = Matrix(elems.clone: _*)
+  def pow(a: Int): Matrix[T] = {
+    var result = this.clone
+
+    def pow_rec(a: Int): Matrix[T] = {
+      if (a == 0) id
+      else {
+        result = pow_rec(a/2)
+        result = result * result
+        if (a % 2 == 1) result *= this
+        result
+      }
+    }
+
+    pow_rec(a)
+  }
+
+  def map(f: (Int, Int, T) => T): Matrix[T] = {
+    val result = new Matrix(new Array[Row[T]](dim): _*)
+    for (i <- result.rows.indices) {
+      result.rows(i) = Row(new Array[T](dim): _*)
+      for (j <- result.rows(i).elems.indices)
+        result.rows(i).elems(j) = f(i,j,this(i,j))
+    }
+    result
+  }
+
+  def id: Matrix[T] = {map {
+    (i,j,x) => if (i==j) one else zero
+  }}
+
+  override def toString: String = {
+    val result = (for (r <- rows) yield r.toString).mkString("\n")
+    result
+  }
+
+  override def clone = Matrix(rows.clone: _*)
 }
 
 
--- matrices1/src/main/scala/Row.scala
+++ matrices1/src/main/scala/Row.scala
@@ -3,14 +3,16 @@ import scala.reflect.ClassTag
 /**
   * Created by noname on 11.03.16.
   */
-case class Row[T](elems: T*)(implicit fractional: Fractional[T], tag: ClassTag[T]) {
+case class Row[T](e: T*)(implicit fractional: Fractional[T], tag: ClassTag[T]) {
   import fractional._
   import tag._
+
+  val elems = e.toArray
 
   def this(e: Array[T])(implicit fractional: Fractional[T], tag: ClassTag[T]) =
     this(e: _*)
 
-  def this(dim: Int) = {
+  def this(dim: Int)(implicit fractional: Fractional[T], tag: ClassTag[T]) = {
     this(new Array[T](dim))
   }
 
@@ -20,10 +22,9 @@ case class Row[T](elems: T*)(implicit fractional: Fractional[T], tag: ClassTag[T
 
   def /(a: T): Row[T] = Row(elems.map((x) => x / a): _*)
 
-  def +(r: Row[T]): Row[T] = Row(elems.zip(r.elems).map { case (x,y) => x+y} : _*)
+  def +(r: Row[T]): Row[T] = Row(elems.zip(r.elems).map { case (x,y) => x+y } : _*)
 
-  def -(r: Row[T]): Row[T] = Row(elems.zip(r.elems).map { case (x,y) => x-y} : _*)
-
+  def -(r: Row[T]): Row[T] = Row(elems.zip(r.elems).map { case (x,y) => x-y } : _*)
   def apply(i: Int): T = elems(i)
 
   def norm: T = elems.maxBy(abs(_))
