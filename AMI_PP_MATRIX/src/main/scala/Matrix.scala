1d1d11aa8037cf17537e2aef73ac43fcc9118047
Fedorov Igor <fedorov.igor95@gmail.com>
2016-04-15 21:21:08 +0400

    * Generic matrix * New bugs

--- AMI_PP_MATRIX/src/main/scala/Matrix.scala
+++ AMI_PP_MATRIX/src/main/scala/Matrix.scala
@@ -1,64 +1,68 @@
 import algebra.Structures._
-import algebra.Instances._
 
 object Matrix {
-  implicit val doubleMatrixRing = new Ring[Matrix[Double]] {
+  implicit def matrixRing[T](implicit F: Field[T], m2: Manifest[T]) = new Ring[Matrix[T]] {
+
     def id = ???
+
     def unit = ???
-    def append(a: Matrix[Double], b: => Matrix[Double]): Matrix[Double] = {
+
+    def append(a: Matrix[T], b: => Matrix[T]): Matrix[T] = {
       val n = a().length
-      val na = new Array[Array[Double]](n)
+      val na = Array.fill(n)(Array.fill(n)(F.id))
       for (i <- 0 until n) {
-        na(i) = new Array[Double](n)
-        for (j <- 0 until n) na(i)(j) = a(i)(j) + b(i)(j)
+        for (j <- 0 until n) na(i)(j) = a(i)(j) |+| b(i)(j)
       }
       Matrix(na)
     }
-    def subtract(a: Matrix[Double], b: => Matrix[Double]): Matrix[Double] = {
+
+    def subtract(a: Matrix[T], b: => Matrix[T]): Matrix[T] = {
       val n = a().length
-      val na = new Array[Array[Double]](n)
+      val na = Array.fill(n)(Array.fill(n)(F.id))
       for (i <- 0 until n) {
-        na(i) = new Array[Double](n)
-        for (j <- 0 until n) na(i)(j) = a(i)(j) - b(i)(j)
+        for (j <- 0 until n) na(i)(j) = a(i)(j) |-| b(i)(j)
       }
       Matrix(na)
     }
-    def multiply(a: Matrix[Double], b: => Matrix[Double]): Matrix[Double] = {
-      val na = new Array[Array[Double]](a().length)
+
+    def multiply(a: Matrix[T], b: => Matrix[T]): Matrix[T] = {
+      val n = a().length
+      val na = Array.fill(n)(Array.fill(n)(F.id))
       for (i <- 0 until a().length) {
-        na(i) = new Array[Double](b().length)
         for (j <- 0 until b().length)
           for (k <- 0 until a(0).length)
-            na(i)(j) += a(i)(k) + b(k)(j)
+            na(i)(j) = na(i)(j) |+| a(i)(k) |+| b(k)(j)
       }
       Matrix(na)
     }
   }
 
-  def |*|(a: Double, b: => Matrix[Double]): Matrix[Double] = Matrix(b().map(_.map(_ * a)))
+  def transpose[T](a: Matrix[T])(implicit F: Field[T], m1: Manifest[Array[T]]) = Matrix(a().transpose)
 
-  def transpose(a: Matrix[Double]) = Matrix(a().transpose)
+  def isDiag[T](a: Matrix[T])(implicit F: Field[T], m1: Manifest[T]) = a == transpose(a)
 
-  def isSquare(a: Matrix[Double]) = a().length == a(0).length
+  def isZero[T](a: Matrix[T])(implicit F: Field[T], m1: Manifest[T]) = a == zero(a().length)
 
-  def isDiag(a: Matrix[Double]) = a == transpose(a)
+  def zero[T](n: Int)(implicit F: Field[T], m1: Manifest[T]) = Matrix(Array.fill(n)(Array.fill(n)(F.id)))
 
-  def isZero(a: Matrix[Double]) = a == zero(a().length)
+  def isSquare[T](a: Matrix[T])(implicit F: Field[T], m1: Manifest[T]) = a().length == a(0).length
 
-  def zero(n: Int) = Matrix(Array.fill(n)(Array.fill(n)(0.0)))
-
-  def id(n: Int):Matrix[Double] = {
-    val na = Array.fill(n)(Array.fill(n)(0.0))
-    for (i <- 0 until n) na(i)(i) = 1.0
+  def id[T](n: Int)(implicit F: Field[T], m1: Manifest[T]): Matrix[T] = {
+    val na = Array.fill(n)(Array.fill(n)(F.id))
+    for (i <- 0 until n) na(i)(i) = F.unit
     Matrix(na)
   }
-  def isId(a: Matrix[Double]) = {
-     (a |-| id(a().length)) == zero(a().length)
-   }
+
+  def isId[T](a: Matrix[T])(implicit F: Field[T], m1: Manifest[T]) = {
+    (a |-| id(a().length)) == zero(a().length)
+  }
 }
 
-case class Matrix[T](elems: Array[Array[T]])(implicit f: Field[T], m: Manifest[Array[Array[Double]]]) {
+case class Matrix[T](elems: Array[Array[T]])(implicit f: Field[T], m: Manifest[Array[Array[T]]]) {
+
   def apply() = elems
+
   def apply(i: Int) = elems(i)
+
   override def toString() = elems.map(x => x.mkString("(", ", ", ")")).mkString("\n")
 }
